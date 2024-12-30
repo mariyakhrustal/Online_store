ba56a724deca7337efd72b6374fed14b179ecd0a
Mariya Khrustaleva <mbotmanova@gmail.com>
2024-12-30 16:50:04 +0500

    classes test

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+
+from src.category import Category
+from src.product import Product
+
+
+@pytest.fixture
+def product_example():
+    return Product("bottle", "plastic", 100.0, 2)
+
+
+@pytest.fixture
+def category_example1():
+    return Category("packaging", "packaging for drinks", [])
+
+
+@pytest.fixture
+def category_example2():
+    return Category(
+        "packaging",
+        "packaging for milk",
+        products=[
+            Product("bottle", "glass", 200.0, 1),
+            Product("Tetra Pak", "cardboard", 300.0, 4),
+            Product("bottle", "plastic", 100.0, 3),
+        ],
+    )
